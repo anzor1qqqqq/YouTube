0f5affc9e82acb9b4b48fcc8a359508804f53dd2
Anzor <anzor.shadzhe@gmail.com>
2023-01-17 13:38:06 +0300

    modified:   css/style.css 	modified:   js/script.js

--- js/script.js
+++ js/script.js
@@ -1,25 +1,62 @@
 'use strict'
+
 new Swiper('.video_recomen_all_user', {
     loop: true,
-    slidesPerView: 3, 
+    slidesPerView: 1, 
     spaceBetween: 43,  
-
 
     navigation: {
       nextEl: '.next',
       prevEl: '.prev',
     },
+
+    breakpoints: {
+      // when window width is >= 640px
+      1100: {
+        slidesPerView: 2,
+       /*  spaceBetween: 20, */
+      },
+      1600: {
+        slidesPerView: 3,
+      },
+    }
 });  
 
 new Swiper('.user_recomen_video', {
     loop: true,
-    slidesPerView: 6,
+    slidesPerView: 1,
     spaceBetween: 20,
-
 
     navigation: {
       nextEl: '.next',
       prevEl: '.prev',
     },
+
+    breakpoints: {
+      // when window width is >= 640px
+      800: {
+        slidesPerView: 2,
+       /*  spaceBetween: 20, */
+      },
+
+      1100: {
+        slidesPerView: 3,
+       /*  spaceBetween: 20, */
+      },
+
+      1300: {
+        slidesPerView: 4,
+       /*  spaceBetween: 20, */
+      },
+
+      1600: {
+        slidesPerView: 5,
+       /*  spaceBetween: 20, */
+      },
+      1900: {
+        slidesPerView: 6,
+        /* spaceBetween: 20, */
+      },
+    }
 });
 
